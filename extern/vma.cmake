1033fe304d3af5257ec2634f46ff63d4d95c3f05
ja50n <zs_feng@qq.com>
2024-08-12 22:22:25 +0800

    add external lib as git submodules

---
+++ extern/vma.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 3.5)
+
+project(vma
+  LANGUAGES CXX
+  DESCRIPTION "VMA as library."
+  HOMEPAGE_URL "https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator"
+)
+
+add_library(${PROJECT_NAME}
+  STATIC
+  ./vma.cpp
+)
+
+set(ENV{VULKAN_SDK} "C:/VulkanSDK/1.3.290.0")
+find_package(Vulkan REQUIRED)
+
+target_include_directories(${PROJECT_NAME} PRIVATE
+  VMA/include
+  ${Vulkan_INCLUDE_DIRS}
+)
